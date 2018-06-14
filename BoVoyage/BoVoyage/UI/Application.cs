f9d178a4ed5d9c896fa872be780e468770aa7bb5
Jessy <jessy@laniel.me>
2018-06-14 14:16:29 +0200

    bbbb

--- BoVoyage/BoVoyage/UI/Application.cs
+++ BoVoyage/BoVoyage/UI/Application.cs
@@ -4,66 +4,20 @@ using System;
 
 namespace BoVoyage.UI
 {
-    public class Application
+    /*public class Application
     {
-        private Menu menuPrincipal;
-        private ModuleGestionClients moduleGestionClients;
-        private ModuleGestionVoyages moduleGestionVoyages; 
-        private ModuleGestionDossiersReservations moduleGestionDossiersReservations;
+        private MenuPrincipal;
+        private GestionVoyage gestionVoyage; 
+        private GestionDossierReservation gestionDossierReservation;
 
-        private ModuleGestionClients ModuleGestionClients
+        public GestionVoyage GetGestionVoyage
         {
-            get => this.moduleGestionClients;
+            get => this.gestionVoyage;
         }
 
-        public ModuleGestionVoyages ModuleGestionVoyages
+        public GestionDossierReservation GestionDossierReservation
         {
-            get => this.moduleGestionVoyages;
+            get => this.gestionDossierReservation;
         }
-
-        public ModuleGestionDossiersReservations ModuleGestionDossiersReservations
-        {
-            get => this.moduleGestionDossiersReservations;
-        }
-
-        private void InitialiserModules()
-        {
-            this.moduleGestionClients = new ModuleGestionClients();
-            this.moduleGestionVoyages = new ModuleGestionVoyages();
-            this.moduleGestionDossiersReservations = new ModuleGestionDossiersReservations();
-        }
-
-        private void InitialiserMenuPrincipal()
-        {
-            this.menuPrincipal = new Menu("Menu Principal");
-            this.menuPrincipal.AjouterElement(new ElementMenu("1", "Gestion des dossiers de reservation")
-            {
-                AfficherLigneRetourMenuApresExecution = false,
-                FonctionAExecuter = this.moduleGestionDossiersReservations.Demarrer
-            });
-            this.menuPrincipal.AjouterElement(new ElementMenu("2", "Gestion des voyages")
-            {
-                AfficherLigneRetourMenuApresExecution = false,
-                FonctionAExecuter = this.moduleGestionVoyages.Demarrer
-            });
-            this.menuPrincipal.AjouterElement(new ElementMenu("3", "Gestion des clients")
-            {
-                AfficherLigneRetourMenuApresExecution = false,
-                FonctionAExecuter = this.moduleGestionClients.Demarrer
-            });
-            this.menuPrincipal.AjouterElement(new ElementMenuQuitterMenu("Q", "Quitter")
-            {
-                FonctionAExecuter = () => Environment.Exit(1)
-            });
-        }
-
-        public void Demarrer()
-        {
-            this.InitialiserModules();
-            this.InitialiserMenuPrincipal();
-
-            this.menuPrincipal.Afficher();
-        }
-
     }
 }
