a9cd10adf70812183f7464a707f427d31259d2fd
Jessy <jessy@laniel.me>
2018-06-14 11:35:59 +0200

    Dossier Frameworks avec les menus

---
+++ BoVoyage/BoVoyage/Frameworks/InformationAffichageAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Frameworks
+{
+
+    public sealed class InformationAffichageAttribute : Attribute
+    {
+        public int NombreCaracteres { get; set; }
+
+        public string Entete { get; set; }
+    }
+
+}
---
+++ BoVoyage/BoVoyage/UI/GestionDossierReservation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.UI
+{
+    class Gestion_des_réservations
+    {
+    }
+}
